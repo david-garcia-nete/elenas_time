4cb614d83c208c5039dd69f79670db7efc346470
david-garcia-nete <david.garcia.nete@gmail.com>
2014-07-28 09:59:05 -0400

    First commit
    First commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<link href='http://fonts.googleapis.com/css?family=Pinyon+Script' rel='stylesheet' type='text/css'>
+<body style =" background-image:url('http://33.media.tumblr.com/tumblr_mb4bfgqm7j1rnog2ho1_500.gif'); background-repeat:repeat;">
+<!--<img class="post_media_photo image" alt="" width="500" height="352" src="http://33.media.tumblr.com/tumblr_mb4bfgqm7j1rnog2ho1_500.gif">-->
+<audio controls>
+    <source src="Laurelhouse - The River by brian doerksen.mp3" type="audio/mpeg">
+    <source src="Laurelhouse - The River by brian doerksen.ogg" type="audio/ogg">
+    <embed height="50" width="100" src="Laurelhouse - The River by brian doerksen.mp3">
+</audio>
+<br>
+<audio controls>
+    <source src="hijitobb - CRISTO FUE UN HOMBRE SINGULAR.mp3" type="audio/mpeg">
+    <source src="hijitobb - CRISTO FUE UN HOMBRE SINGULAR.ogg" type="audio/ogg">
+    <embed height="50" width="100" src="hijitobb - CRISTO FUE UN HOMBRE SINGULAR.mp3">
+</audio>
+<br>
+<audio controls>
+    <source src="Lucero Pereyra M. - Hija de un Rey (Letra) Sud.mp3" type="audio/mpeg">
+    <source src="Lucero Pereyra M. - Hija de un Rey (Letra) Sud.ogg" type="audio/ogg">
+    <embed height="50" width="100" src="Lucero Pereyra M. - Hija de un Rey (Letra) Sud.mp3">
+</audio>
+<br>
+<audio controls>
+    <source src="La Prueba es Vivir - Lds.mp3" type="audio/mpeg">
+    <source src="La Prueba es Vivir - Lds.ogg" type="audio/ogg">
+    <embed height="50" width="100" src="La Prueba es Vivir - Lds.mp3">
+</audio>
+<br>
+<audio controls>
+    <source src="Jorge Mauricio Cajamarca Aldana - YO SE QUE VIVE MI SENOR !!!.mp3" type="audio/mpeg">
+    <source src="Jorge Mauricio Cajamarca Aldana - YO SE QUE VIVE MI SENOR !!!.ogg" type="audio/ogg">
+    <embed height="50" width="100" src="Jorge Mauricio Cajamarca Aldana - YO SE QUE VIVE MI SENOR !!!.mp3">
+</audio>
+<br>
+<audio controls>
+    <source src="victorkary - sus manos (en espanol).mp3" type="audio/mpeg">
+    <source src="victorkary - sus manos (en espanol).ogg" type="audio/ogg">
+    <embed height="50" width="100" src="victorkary - sus manos (en espanol).mp3" >
+</audio>
+<center><h1><span style="color:white;font-size:200%;font-family: 'Pinyon Script', cursive;">Elena's Time</span></h1></center>
+
+
+
+</body>
+</html>
