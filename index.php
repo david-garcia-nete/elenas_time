414a3137869a66497bf412b24dc7a570f974a0cb
david-garcia-nete <david.garcia.nete@gmail.com>
2014-07-28 21:12:53 -0400

    Added twitter boostrap template.

--- index.php
+++ index.php
@@ -1,8 +1,25 @@
 <!DOCTYPE html>
-<html>
-<link href='http://fonts.googleapis.com/css?family=Pinyon+Script' rel='stylesheet' type='text/css'>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>A personal Web site for my friend Elena.</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <!-- Optional theme -->
+<!--    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">-->
+    <link href='http://fonts.googleapis.com/css?family=Pinyon+Script' rel='stylesheet' type='text/css'>
+
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
 <body style =" background-image:url('http://33.media.tumblr.com/tumblr_mb4bfgqm7j1rnog2ho1_500.gif'); background-repeat:repeat;">
-<!--<img class="post_media_photo image" alt="" width="500" height="352" src="http://33.media.tumblr.com/tumblr_mb4bfgqm7j1rnog2ho1_500.gif">-->
 <audio controls>
     <source src="Laurelhouse - The River by brian doerksen.mp3" type="audio/mpeg">
     <source src="Laurelhouse - The River by brian doerksen.ogg" type="audio/ogg">
@@ -40,7 +57,9 @@
 </audio>
 <center><h1><span style="color:white;font-size:200%;font-family: 'Pinyon Script', cursive;">Elena's Time</span></h1></center>
 
-
-
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 </body>
 </html>
