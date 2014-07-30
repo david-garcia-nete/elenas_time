6905a2a143e6a6b6d293e668323e194dfeb9830b
david-garcia-nete <david.garcia.nete@gmail.com>
2014-07-29 21:56:42 -0400

    Add text intro and favicon.

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="favicon.ico">
     <title>A personal Web site for my friend Elena.</title>
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
@@ -18,16 +19,24 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-</head>
 <body style =" background-image:url('http://33.media.tumblr.com/tumblr_mb4bfgqm7j1rnog2ho1_500.gif'); background-repeat:repeat;">
-<div id="cp_widget_f6f75756-fe3e-4d53-b7a5-7d07ce5d9d02">...</div><script type="text/javascript">
-    var cpo = []; cpo["_object"] ="cp_widget_f6f75756-fe3e-4d53-b7a5-7d07ce5d9d02"; cpo["_fid"] = "AILAT1LFaije";
+<div id="cp_widget_2c70324d-80e1-4ff1-92fc-23aa54fd3fc8">...</div><script type="text/javascript">
+    var cpo = []; cpo["_object"] ="cp_widget_2c70324d-80e1-4ff1-92fc-23aa54fd3fc8"; cpo["_fid"] = "AILAT1LFaije";
     var _cpmp = _cpmp || []; _cpmp.push(cpo);
     (function() { var cp = document.createElement("script"); cp.type = "text/javascript";
         cp.async = true; cp.src = "//www.cincopa.com/media-platform/runtime/libasync.js";
         var c = document.getElementsByTagName("script")[0];
         c.parentNode.insertBefore(cp, c); })(); </script><noscript>Powered by Cincopa <a href='http://www.cincopa.com/video-hosting'>Video Streaming Hosting</a> solution.<span>New Gallery 2014/7/29</span></noscript>
-<center><h1><span style="color:white;font-size:200%;font-family: 'Pinyon Script', cursive;">Elena's Time</span></h1></center>
+
+<center><h1><span style="background-color:black;color:white;font-size:200%;font-family: 'Pinyon Script', cursive;">Elena's Time</span></h1></center>
+<p  style="background-color:black;color:white;font-size:200%;font-family: 'Pinyon Script', cursive;">Hola a todos mi nombre es Elena Castaneda y fuí muy bendecida creci en un hogar maravilloso y humilde con buenos principios, soy la número cuatro de los hijos de mi madre somos tres chicas y un chico conoci el evangelio siendo aun muy pequeña por medio de mi abuelo quien tambien era mi padre una noche trajo consigo a los misioneros quienes trajieron un mensaje hermoso sobre ese bello plan de salvación, y quien diria que llego para quedarse pues todos en casa lo conocemos y es una bendición vivirlo.
+    Tengo 24 años y e disfrutado de las bendicones del evanegelio por muchos de esos 24 años, me gusta bailar mas que cantar, jugar futból, la comida y cultura arabe. Yo creo que nunca es tarde para aprender algo Nuevo. En casa aprendi que estamos aqui para servir a los demás y eso es lo que trato de hacer. También tengo un testimonio sobre el libro de mormón: si es el libro que cambio mi vida, trajo paz a mi corazón y alimento mi espiritu me lleno de conocimiento de las cosas que mi padre celestial quiere que haga aqui. Amo con todo mi Corazon esta iglesia es verdadera que el profeta jose smith fue un profeta de Dios que el libro de Mormon es verdadero y que el temple es la casa de Dios sin duda se que mi padre celestial me ama y yo le amo a el.
+</p>
+<p  style="background-color:black;color:white;font-size:200%;font-family: 'Pinyon Script', cursive;">
+    Hey! Cambio mi vida y la de muchas personas, quieres conocer cuantos somos felices viviendolo?
+    Quedate aqui y conoce a alguien como tu y como yo y dejemos que nos cuente como el evangelio cambio su vida.
+</p>
+
 
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
