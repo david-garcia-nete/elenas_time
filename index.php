21af5d0a808f3dd96a179a4573ba8da7ad0ab74f
david-garcia-nete <david.garcia.nete@gmail.com>
2014-07-30 12:21:59 -0400

    Implement translate button.

--- index.php
+++ index.php
@@ -20,26 +20,47 @@
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 <body style =" background-image:url('http://33.media.tumblr.com/tumblr_mb4bfgqm7j1rnog2ho1_500.gif'); background-repeat:repeat;">
-<!--<div id="cp_widget_2c70324d-80e1-4ff1-92fc-23aa54fd3fc8">...</div><script type="text/javascript">-->
-<!--    var cpo = []; cpo["_object"] ="cp_widget_2c70324d-80e1-4ff1-92fc-23aa54fd3fc8"; cpo["_fid"] = "AILAT1LFaije";-->
-<!--    var _cpmp = _cpmp || []; _cpmp.push(cpo);-->
-<!--    (function() { var cp = document.createElement("script"); cp.type = "text/javascript";-->
-<!--        cp.async = true; cp.src = "//www.cincopa.com/media-platform/runtime/libasync.js";-->
-<!--        var c = document.getElementsByTagName("script")[0];-->
-<!--        c.parentNode.insertBefore(cp, c); })(); </script><noscript>Powered by Cincopa <a href='http://www.cincopa.com/video-hosting'>Video Streaming Hosting</a> solution.<span>New Gallery 2014/7/29</span></noscript>-->
-
+<div id="cp_widget_3fd5b305-11ab-46f8-bfc7-078cca5d248f">...</div><script type="text/javascript">
+    var cpo = []; cpo["_object"] ="cp_widget_3fd5b305-11ab-46f8-bfc7-078cca5d248f"; cpo["_fid"] = "AILAT1LFaije";
+    var _cpmp = _cpmp || []; _cpmp.push(cpo);
+    (function() { var cp = document.createElement("script"); cp.type = "text/javascript";
+        cp.async = true; cp.src = "//www.cincopa.com/media-platform/runtime/libasync.js";
+        var c = document.getElementsByTagName("script")[0];
+        c.parentNode.insertBefore(cp, c); })(); </script><noscript>Powered by Cincopa <a href='http://www.cincopa.com/video-hosting'>Video Streaming Hosting</a> solution.<span>New Gallery 2014/7/29</span><span>Cristo Fué Un Hombre Singular</span><span>Yo Sé Que Vive Mi Señor</span><span>La Prueba es Vivir</span><span>The River</span><span>Hija De Un Rey</span><span>Sus Manos</span></noscript>
 <center><h1><span style="background-color:black;color:white;font-size:200%;font-family: 'Pinyon Script', cursive;">Brothers and Sisters</span></h1></center>
-<p  style="background-color:black;color:white;font-size:200%;font-family: 'Pinyon Script', cursive;">Hola a todos mi nombre es Elena Castaneda y fuí muy bendecida creci en un hogar maravilloso y humilde con buenos principios, soy la número cuatro de los hijos de mi madre somos tres chicas y un chico conoci el evangelio siendo aun muy pequeña por medio de mi abuelo quien tambien era mi padre una noche trajo consigo a los misioneros quienes trajieron un mensaje hermoso sobre ese bello plan de salvación, y quien diria que llego para quedarse pues todos en casa lo conocemos y es una bendición vivirlo.
+<button style="font-family: 'Pinyon Script', cursive;font-size:200%;" id = "translate" value = "spanish" type="button" onclick="translator (this.value)">English</button>
+<p class="spanish" style="background-color:black;color:white;font-size:200%;font-family: 'Pinyon Script', cursive;">Hola a todos mi nombre es Elena Castaneda y fuí muy bendecida creci en un hogar maravilloso y humilde con buenos principios, soy la número cuatro de los hijos de mi madre somos tres chicas y un chico conoci el evangelio siendo aun muy pequeña por medio de mi abuelo quien tambien era mi padre una noche trajo consigo a los misioneros quienes trajieron un mensaje hermoso sobre ese bello plan de salvación, y quien diria que llego para quedarse pues todos en casa lo conocemos y es una bendición vivirlo.
         Tengo 24 años y e disfrutado de las bendicones del evanegelio por muchos de esos 24 años, me gusta bailar mas que cantar, jugar futból, la comida y cultura arabe. Yo creo que nunca es tarde para aprender algo Nuevo. En casa aprendi que estamos aqui para servir a los demás y eso es lo que trato de hacer. También tengo un testimonio sobre el libro de mormón: si es el libro que cambio mi vida, trajo paz a mi corazón y alimento mi espiritu me lleno de conocimiento de las cosas que mi padre celestial quiere que haga aqui. Amo con todo mi Corazon esta iglesia es verdadera que el profeta jose smith fue un profeta de Dios que el libro de Mormon es verdadero y que el temple es la casa de Dios sin duda se que mi padre celestial me ama y yo le amo a el.
-    </p><p  style="background-color:black;color:white;font-size:200%;font-family: 'Pinyon Script', cursive;">
+    </p>
+<p class="spanish"  style="background-color:black;color:white;font-size:200%;font-family: 'Pinyon Script', cursive;">
         Hey! Cambio mi vida y la de muchas personas, quieres conocer cuantos somos felices viviendolo?
         Quedate aqui y conoce a alguien como tu y como yo y dejemos que nos cuente como el evangelio cambio su vida.
     </p>
 
+<p class="english" style="display:none;background-color:black;color:white;font-size:200%;font-family: 'Pinyon Script', cursive;">Hello everyone my name is Elena Castaneda and I was very blessed grew up in a wonderful, humble home with good principles, I am number four of the sons of my mother we are three girls and a boy I kno the gospel being still very small by my grandfather who was also my father one night brought the missionaries who trajieron a beautiful message on this beautiful plan of salvation, and who would say that they all get to stay at home and we know it is a blessing to live it. 24 years I have enjoyed the e bendicones the evanegelio for many of those 24 years, I like dancing more than singing, playing soccer, food and Arabic culture. I think it's never too late to learn something new. At home I learned that we are here to serve others and that's what I try to do. I also have a testimony of the Book of Mormon: if it is the book that changed my life, brought peace to my heart and my spirit feed me full of knowledge of the things that my Heavenly Father wants me to do here. Love with all my Heart this church is true that the Prophet Joseph Smith was a prophet of God that the book of Mormon is true and that the temple is the house of God no doubt that my Heavenly Father loves me and I love the .
+</p>
+<p class="english"  style="display:none;background-color:black;color:white;font-size:200%;font-family: 'Pinyon Script', cursive;">
+    Hey! Change my life and many people want to know how many are happy living it? Stay here and meet somebody like you and me and let them tell us how the gospel changes your life.
+</p>
+
 
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <!-- Latest compiled and minified JavaScript -->
 <!--<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>-->
+<script>
+function translator (current_language){
+    if (current_language == "spanish"){
+        $(".spanish").hide();
+        $(".english").show();
+        $("#translate").html("Español").val("english");
+    }
+    if (current_language == "english"){
+        $(".english").hide();
+        $(".spanish").show();
+        $("#translate").html("English").val("spanish");
+    }
+}
+</script>
 </body>
 </html>
